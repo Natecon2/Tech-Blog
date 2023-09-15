36ee8aaff5c9c1eba2c87f8d0365531827aadb2d
Natecon2 <frankenfieldnathan5@gmail.com>
2023-09-14 19:12:59 -0700

    changed controller file names

---
+++ server.js
@@ -0,0 +1,37 @@
+//const express = require('express');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware setup
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Session configuration
+const sess = {
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+};
+
+app.use(session(sess));
+
+app.use(routes);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ error: 'Server error' });
+});
+
+// Start the server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+});
